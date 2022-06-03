cda33fd57db5cff8ced04ee619a80e268e8c6e6d
mehni <lozomehni28@gmail.com>
2022-06-03 19:05:33 +0100

    fix app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-import express,{Request,Response,NextFunction,Application} from 'express';
+import {Request,Response,NextFunction,Application} from 'express';
+const express = require('express');
 const bodyParser = require("body-parser");
 import cors from 'cors';
-
-
 const app: Application = express();
 
 app.use(bodyParser.json({limit:'20mb'}));
-app.use(bodyParser.urlendcoded({limit:'20mb',extended:true,parameterLimit:50000}));
+app.use(bodyParser.urlencoded({limit:'20mb',extended:true,parameterLimit:50000}));
 //app.use(cors({origin:corsUrl,optionsSuccessStatus:200}))
 
 const port = process.env.PORT || 3000;
